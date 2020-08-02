5b2a7a79fad773d4b923f9e80322045b09279c1b
Jitendar Chauhan <aryanrajput278@gmail.com>
2020-08-02 15:45:18 +0530

    Added OneToOne relationship

--- relations/Relationships.js
+++ relations/Relationships.js
@@ -1,5 +1,17 @@
 class Relationships {
 
+    hasOne(From, To, f_id, l_id) {
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM ${To.table} where ${f_id}=${this[l_id]}`
+            this.constructor.connection.query(query, (e, r, f) => {
+                if (e) reject(e)
+                else{
+                    resolve(To.dto(r,false))
+                }
+            })
+        })
+    }
+
     hasMany(From, To, f_id, l_id) {
         return new Promise((resolve, reject) => {
             const query = `SELECT * FROM ${To.table} where ${f_id}=${this[l_id]}`
@@ -23,6 +35,7 @@ class Relationships {
             })
         })
     }
+
 
 }
 
