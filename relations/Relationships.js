7c49bed3d68292a4aa83f6b343d05d870d4417fe
Jitendar Chauhan <aryanrajput278@gmail.com>
2020-08-02 16:26:34 +0530

    Added ManyToMany relationship

--- relations/Relationships.js
+++ relations/Relationships.js
@@ -36,6 +36,18 @@ class Relationships {
         })
     }
 
+    belongsToMany(From, To, From_f_id, To_f_id, pivot_table, f_id = "id", t_id = "id") {
+        return new Promise((resolve, reject) => {
+            const query = `SELECT * FROM ${To.table} where ${t_id} IN ( select ${To_f_id}  from ${ pivot_table } where ${From_f_id} = ${this[f_id]} )`
+            this.constructor.connection.query(query, (e, r, f) => {
+                if (e) reject(e)
+                else{
+                    resolve(To.dto(r))
+                }
+            })
+        })
+    }
+
 
 }
 
