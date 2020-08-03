69c8adf9d7ddc056e9e33ab0d5e37907adfdfc95
Kuldeep Kalyan <kuldeep.kalyan@symbtechnologies.com>
2020-08-03 11:36:51 +0530

    Fixed AVG function

--- utilities/Collection.js
+++ utilities/Collection.js
@@ -1,34 +1,35 @@
 const { connection } = require('../nodeorm')
 
 class Collection {
+
     constructor(items = []) {
         this.items = items
     }
 
-    size(){
+    size() {
         return this.items.length
     }
 
-    reverse(){
+    reverse() {
         return this.items.reverse()
     }
 
-    sort(type = 'asc'){
+    sort(type = 'asc') {
         return this.items.sort(function (a, b) {
-            return type.toLowerCase() ==='asc' ? a > b ? 1:-1: b > a ? 1 : -1
+            return type.toLowerCase() === 'asc' ? a > b ? 1 : -1 : b > a ? 1 : -1
         })
-    }
-    avg(){
-        return this.items.reduce(function(sum, item){
-            return sum + item
-        }) / this.size()
     }
 
-    sum(){
-        return this.items.reduce(function(sum, item){
+    avg() {
+        return this.sum() / this.size()
+    }
+
+    sum() {
+        return this.items.reduce(function (sum, item) {
             return sum + item
         })
     }
+
 }
 
 module.exports = { Collection }
